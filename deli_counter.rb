68b12f4889e05217498c872d0e8d6d5ebe7d0f83
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 20:23:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,22 +4,3 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   katz_deli.size
 end
-
-def now_serving(katz_deli)
-  if katz_deli.size == 0
-    puts "There is nobody waiting to be served!"
-  else
-    serving = array [0]
-    puts "Currently serving #{katz-deli}."
-  katz_deli.shift
-  end
-end
-
-def line(katz_deli)
-  if array.size === 0
-  puts "The line is currently empty."
-else
-  katz_deli.each_with_index do |name, index|
-    puts "#{index+1}. #{name} "
-  end
-end
