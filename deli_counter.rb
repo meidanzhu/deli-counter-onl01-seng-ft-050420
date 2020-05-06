aee6d8ef7974fdefacb3f782e53d832315093e43
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 20:48:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,10 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-  message="The line is currently:"
+  message = "The line is currently:"
   katz_deli.each_with_index do |name, index|
   message += " #{index+1}. #{name}"
   end
   puts "#{message}"
+  end
 end
