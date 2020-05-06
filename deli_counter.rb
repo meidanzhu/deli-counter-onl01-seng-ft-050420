3f14bae278cf306f569d9ddf261170662ecde870
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 20:53:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,17 @@ def line(katz_deli)
   puts "#{message}"
   end
 end
+
+def take_a_number(katz_deli,name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current_customer = katz_deli.shift
+    puts "Currently serving #{current_customer}"
+  end
+end
