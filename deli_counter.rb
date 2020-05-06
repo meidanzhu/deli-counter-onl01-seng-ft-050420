b3a6f96546000e750809855777d02828aa8f7efc
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 20:34:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,25 @@
-def line(katz_deli)
-  if array.size == 0
-    puts "There is nobody waiting to be served!"
-end
-
-  def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are #{array.size} in line."
+def take_a_number(array, name)
+  array << name
+  puts name, (array.index(name)+1)
 end
 
 def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
     serving = array[0]
     puts "Current serving #{serving}."
     array.shift
+  end
+end
+
+def line(array)
+  if array.size == 0
+    puts "The Line is currently empty."
+  else
+    print "The line is currently: "
+    for i in 1..array.size
+      print "#{i}. #{array[i-1]} "
+    end
   end
 end
