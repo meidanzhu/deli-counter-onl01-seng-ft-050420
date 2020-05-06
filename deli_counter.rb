9b320a1d2a125966e730129cdf3d06d9eb1dd958
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 20:23:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,15 @@
 katz_deli = []
 
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    serving = array[0]
+    puts "Current serving #{serving}."
+    array.shift
+  end
+end
+
 def take_a_number(katz_deli, name)
   katz_deli << name
   katz_deli.size
