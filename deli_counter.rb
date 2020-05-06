0afc84c24c7d4b02cbb97038615733f8feee4794
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 20:32:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
-katz_deli = []
-
-def now_serving(array)
+def line(katz_deli)
   if array.size == 0
     puts "There is nobody waiting to be served!"
-  else
+end
+
+  def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are #{array.size} in line."
+end
+
+def now_serving(array)
     serving = array[0]
     puts "Current serving #{serving}."
     array.shift
   end
-end
-
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  katz_deli.size
 end
